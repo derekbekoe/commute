92b8b435f52d53e617a2df1570457b70c31bfcc8
Derek Bekoe <debekoe@microsoft.com>
2016-12-01 13:24:29 -0800

    notification support

--- commute-frontend/src/assets/js/custom.js
+++ commute-frontend/src/assets/js/custom.js
@@ -4,6 +4,23 @@
 //     // Sorry! No Web Storage support..
 // }
 
+var Notify = window.Notify.default;
+
+function showCommuteNotification(time_str) {
+    console.log('Showing notification');
+    new Notify('Commute', {
+        body: 'Duration for your commute is '+time_str+'!'
+    }).show();
+}
+
+if (Notify.needsPermission && Notify.isSupported()) {
+    Notify.requestPermission(function() {
+        console.log('Permission has been granted by the user');
+    }, function() {
+        console.warn('Permission has been denied by the user');
+    });
+}
+
 function checkTravelTime(travelTime, travelOrigin, travelDestination) {
     $.get( "http://40.83.187.34:81" + "/travel-time", {origin: localStorage.origin, destination: localStorage.destination}, function( data ) {
     // $.get( "COMMUTE_API" + "/travel-time", {origin: localStorage.origin, destination: localStorage.destination}, function( data ) {
@@ -12,6 +29,10 @@ function checkTravelTime(travelTime, travelOrigin, travelDestination) {
         travelOrigin.text(localStorage.origin)
         travelDestination.text(localStorage.destination)
         travelTime.text(data.traffic_time_str)
+        traffic_mins = data.traffic_time_secs / 60
+        if (traffic_mins > localStorage.notifyMe) {
+            showCommuteNotification(data.traffic_time_str)
+        }
         setTimeout(function(){ checkTravelTime(travelTime, travelLastUpdated); }, 300000)
     });
 }
@@ -34,9 +55,11 @@ function placeAutocompleteList(inputElement, datalist) {
 $(document).ready(function() {
     var origin = $('#custom-origin');
     var destination = $('#custom-destination');
+    var notifyMe = $('#custom-notify-me');
     var travelOrigin = $('#travel-origin');
     var travelDestination = $('#travel-destination');
     var travelTime = $('#travel-time');
+    var travelNotifyMe = $('#travel-notify-me');
     var go = $('#go');
     origin.keydown(function() {
         placeAutocompleteList(origin, $('#origin-datalist'));
@@ -47,13 +70,16 @@ $(document).ready(function() {
     // init the values
     origin.val(localStorage.origin)
     destination.val(localStorage.destination)
+    notifyMe.val(localStorage.notifyMe)
     travelOrigin.text(localStorage.origin)
     travelDestination.text(localStorage.destination)
     go.click(function() {
          localStorage.origin = origin.val()
          localStorage.destination = destination.val()
+         localStorage.notifyMe = notifyMe.val()
          travelOrigin.text(localStorage.origin)
          travelDestination.text(localStorage.destination)
+         travelNotifyMe.text('Notify if time exceeds '+localStorage.notifyMe+' minutes')
          document.location.hash = 'travel'
          checkTravelTime(travelTime, travelOrigin, travelDestination)
     });
